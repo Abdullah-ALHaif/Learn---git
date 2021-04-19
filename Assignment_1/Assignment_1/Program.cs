3f0e4041e22dcd1051e172cbeeceb88d1e0ae7b7
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-04-19 07:50:25 +0300

    Adding Assignments

---
+++ Assignment_1/Assignment_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        class JSBuilder
+        {
+            private string js;
+
+            public JSBuilder()
+            {
+                this.js = "";
+            }
+            public JSBuilder content(string content)
+            {
+                this.js += content;
+                return this;
+            }
+            public JSBuilder function(string function, string data)
+            {
+                this.js += "function " + function + " " + "(" + data + ")" + "{ \n";
+                return this;
+            }
+            public JSBuilder end(string end)
+            {
+                this.js += end + "}";
+                return this;
+            }
+            
+
+
+            public string get()
+            {
+                return this.js;
+            }
+        }
+        static void Main(string[] args)
+        {
+                   
+            JSBuilder js = new JSBuilder();
+
+            string r = js.function("findElement","array, element")
+                .content("var z=false; \n for (var i=0; i<array.length;i++){ \n if (array[i]==element){ \n" +
+                "z=true; \n } \n } \n return z;")
+                .end("\n").get();
+
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ Assignment_2/Assignment_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+namespace Assignment_2
+{
+    class Program
+    {
+        static bool match(string source)
+        {
+            ArrayList list = new ArrayList();
+            int stack = 0;
+            int i = 0;
+            foreach (char ch in source)
+            {
+                
+                list.Add(source[i]);
+                for (int j = source.Length-1 ; j>=0; j--)
+                {
+                    if (source[j]!=ch)
+                    {
+                        stack=1;
+                        
+                    }
+                    else { stack = 0; }
+                }
+                i++;
+            }
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Stack = "+stack);
+            return stack == 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(match("312213"));
+
+        }
+    }
+}
